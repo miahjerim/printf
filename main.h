618fe79f16bfddf63a30b623c2c70e8cef192339
miahjerim <miahjerim@gmail.com>
2023-04-24 16:25:59 +0300

    docs: creat new files including test files main.c stored in test directory

---
+++ main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+int _printf(const char *format, ...);
+void print_buffer(char buffer[], int *buff_ind);
+#endif
---
+++ test/README.md
@@ -0,0 +1,1 @@
+test folder contains all test files for the project
